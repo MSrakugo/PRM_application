803fa79ee6ae5530572bed1a88b859adc5aedb89
Satoshi Matsuno <51268793+MSrakugo@users.noreply.github.com>
2022-03-06 00:39:26 +0900

    Update PRM_Base_220301.py

--- PRM_Base_220301.py
+++ PRM_Base_220301.py
@@ -34,9 +34,9 @@ class Model_feature_setting:
                  setting_PCA, setting_ICA, setting_standard_scaler):
         self.setting_X_raw = setting_X_raw
         self.setting_X_log = setting_X_log
-        self.setting_X_product = setting_X_product 
-        self.setting_X_product_log = setting_X_product_log 
-        
+        self.setting_X_product = setting_X_product
+        self.setting_X_product_log = setting_X_product_log
+
         self.setting_X_ratio = setting_X_ratio
         self.setting_X_ratio_log = setting_X_ratio_log
 
@@ -66,46 +66,46 @@ if raw_data is None:
 else:
     # select location info in uproad dataset
     location_info = st.sidebar.multiselect("Choose the location columns", raw_data.columns, ["DataBase", "SAMPLE_INFO"])
-    
+
     ###### Data check/preprocessing
     PM, Location_Ref_Data = prm.preprocessing_normalize(raw_data, DataBase, SAMPLE_INFO, location_info)
     #st.write(PM)
-    
+
     ###### estimation by PRM
     # model folder
     now_model_folder_name = 'models_220203_ALL_ALL'
-    
+
     # elements list
     elements_list = ['Rb', 'Ba', 'Th', 'U', 'Nb', 'K', 'La', 'Ce', 'Pb', 'Sr', 'Nd', 'Zr', 'Ti', 'Y', 'Yb','Lu', 'SiO2', 'Al2O3', 'MgO', 'Na2O', 'P2O5', 'CaO', 'MnO', 'FeO', 'K2O']
     immobile_elem=st.sidebar.multiselect("Choose the immobile elements (Not Change)", elements_list, ["Zr", "Th", "Ti", "Nb"])
-    mobile_elem=st.sidebar.multiselect("Choose the mobile elements", elements_list, ['Rb', 'Ba', 'Th', 'U', 'Nb', 'K', 'La', 'Ce', 'Pb', 'Sr', 'Nd', 'Zr', 'Ti', 'Y', 'Yb','Lu', 'SiO2', 'Al2O3', 'MgO', 'Na2O', 'P2O5', 'CaO', 'MnO', 'FeO', 'K2O'])
-    
+    mobile_elem=st.sidebar.multiselect("Choose the mobile elements", elements_list, ['Rb', 'Ba', 'Th', 'U', 'Nb', 'K', 'La', 'Ce', 'Pb', 'Sr', 'Nd', 'Zr', 'Ti', 'Y', 'Yb','Lu',])
+
     mobile_data_compile, spidergram_data_compile = prm_predict.predict_protolith(mobile_elem, immobile_elem, PM, Location_Ref_Data, now_model_folder_name)
-    
+
     ###### Data visualization
     choice_sample = st.selectbox('Choice sample',spidergram_data_compile.index, )
     st.write('You selected:', choice_sample)
-    
-    
+
+
     fig, ax = plt.subplots(constrained_layout=True)
     pred_data = pd.DataFrame(spidergram_data_compile.loc[choice_sample]).T.dropna()
     now_col=pred_data.columns
-    
+
     fig=prm.Spidergram_simple(pd.DataFrame(PM.loc[choice_sample]).T[now_col], "log", "off","#344c5c", "--", "off", fig, ax)
     fig=prm.Spidergram_simple(pred_data, "log", "on","#f08575", "-", "off", fig, ax)
     plt.title(choice_sample)
     plt.legend(["Metabasalt comp.", "Protolith comp."])
     st.pyplot(fig)
     ###### Data visualization
-    
-    
+
+
     protolith_data = spidergram_data_compile.copy()
     protolith_data_ppm = prm.PM_to_ppm(protolith_data)
     element_mobility = PM[protolith_data.columns]/protolith_data
-    
-    
+
+
     col1, col2, col3, col4 = st.columns(4)
-        
+
     with col1:
         st.download_button(
             label="Protolith composition predicted by PRM (PM_normalized)",
@@ -119,8 +119,8 @@ else:
             data=protolith_data_ppm.to_csv().encode('utf-8'),
             file_name="Protolith_comp_by_PRM_(ppm)" + DataBase +".csv",
             mime='text/csv',
-            )    
-    
+            )
+
     with col3:
         st.download_button(
             label="Element mobile predicted by PRM (ppm)",
@@ -128,7 +128,7 @@ else:
             file_name="Element_mobile_by_PRM_" + DataBase +".csv",
             mime='text/csv',
             )
-    
+
     with col4:
         st.download_button(
             label="Element mobility predicted by PRM (Metabasalt/Protolith)",
@@ -142,4 +142,3 @@ st.subheader("Cite this article")
 st.caption("Matsuno, S., Uno, M., Okamoto, A. Tsuchiya, N. Machine-learning techniques for quantifying the protolith composition and mass transfer history of metabasalt. Sci Rep 12, 1385 (2022). https://doi.org/10.1038/s41598-022-05109-x")
 
 st.caption("Made by Satoshi Matsuno (Graduate School of Environmental Studies, Tohoku univ.)")
-
